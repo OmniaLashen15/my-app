357150d90e7c28df1337d4843980e4040f7f90d1
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-20 12:12:55 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ExpenseItem from './Components/ExpesneItem';
+import Expenses from './Components/Expenses';
+
 function App() {
+  ///imperative approach
+  // const para = document.createElement("p");
+  // para.textContent = "This is also visible";
+  // document.getElementById('root').append(para);
+  const expenses = [
+    {
+      id:'e1',
+      title:'Toilet Paper',
+      amount:94.12,
+      data:new Date(2021, 7, 14)
+    },
+    { 
+      id:"e2", 
+      title:'New TV',
+      amount:799.49,
+      data:new Date(2021, 2, 12)
+    },
+    {
+      id:'e3',
+      title:'Car Insurance',
+      amount:294.67,
+      data:new Date(2021, 2, 28)
+    },
+    {
+      id:'e4',
+      title:'New Desk (Wooden)',
+      amount:450,
+      data:new Date(2021, 5, 12)}  
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Expenses expenses={expenses}/>
   );
 }
 
---
+++ src/Components/Expenses.js
@@ -0,0 +1,36 @@
+import ExpenseItem from "./ExpesneItem";
+
+function Expenses(props)
+{
+    
+    return(
+        
+        <div className="expenses">
+        <ExpenseItem 
+        title={props.expenses[0].title} 
+        amount={props.expenses[0].amount} 
+        date={props.expenses[0].data}
+        />
+        <ExpenseItem
+   title={props.expenses[1].title} 
+   amount={props.expenses[1].amount} 
+   date={props.expenses[1].data}
+   />
+  <ExpenseItem 
+  title={props.expenses[2].title} 
+  amount={props.expenses[2].amount} 
+  date={props.expenses[2].data}
+  />
+  <ExpenseItem 
+  title={props.expenses[3].title} 
+  amount={props.expenses[3].amount} 
+  date={props.expenses[3].data}
+  />
+</div>
+
+        
+        )
+      
+}
+
+export default Expenses;
