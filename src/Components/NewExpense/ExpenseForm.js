490c96861ff0fb38d8d128c647a89c786eaa68fa
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-22 11:58:24 +0200

    applying two way binding

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -60,11 +60,13 @@ const ExpenseForm = (props) =>
                 <div className="new-expense__control">
                     <label>Title</label>
                     <input type="text" 
+                    value={enteredTitle}  //two way binding
                     onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
                     <input type="number" 
+                    value={enteredAmount}
                     min="0.01" 
                     step="0.01" 
                     onChange={amountChangeHandler}/>
@@ -72,6 +74,7 @@ const ExpenseForm = (props) =>
                 <div className="new-expense__control">
                     <label>Date</label>
                     <input type="date"
+                    value={enteredDate}
                     min="2019-01-01" 
                     max="2022-12-31" 
                     onChange={dateChangeHandler}/>
