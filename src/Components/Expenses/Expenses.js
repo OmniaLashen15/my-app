2ae3813ad3aa4ae293db6a9b390467f1fd7d8a73
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-22 12:47:08 +0200

    add  expenses filter component

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,13 +2,16 @@ import ExpenseItem from "./ExpesneItem";
 import './Expenses.css'
 import Card from "../UI/Card";
 import React from 'react';
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
 
 
 const Expenses = (props) => {
     
     return(
         
-        <Card className="expenses">
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter/>
         <ExpenseItem 
         title={props.expenses[0].title} 
         amount={props.expenses[0].amount} 
@@ -30,6 +33,7 @@ const Expenses = (props) => {
         date={props.expenses[3].data}
         />
         </Card>
+        </div>
         )
       
 }
