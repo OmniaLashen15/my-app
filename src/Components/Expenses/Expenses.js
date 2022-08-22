b758c25f490b611a3d7936479247e75e15b061bc
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-22 19:18:46 +0200

    add expensesList component

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpesneItem";
 import './Expenses.css'
 import Card from "../UI/Card";
 import React,{useState} from 'react';
-import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 
 const Expenses = (props) => {
@@ -17,18 +17,6 @@ const Expenses = (props) => {
       return  item.date.getFullYear().toString() === filteredYear;
   });
 
-    let expensesContent = <p>No expenses found.</p>;
-    if(filteredExpenses.length > 0 ) //conditional content
-    {
-      expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem 
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          />
-      ))
-    }
 
     return (
         <div>
@@ -37,7 +25,7 @@ const Expenses = (props) => {
               selected={filteredYear}
               onChangeFilter={filterChangeHandler}
             />
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
           </Card>
         </div>
       );
